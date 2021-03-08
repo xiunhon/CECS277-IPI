b7af7a2589d0aff7ddf5e5a4ba9089417d536346
xiunhon <80288400+xiunhon@users.noreply.github.com>
2021-03-08 13:34:17 -0800

    Update Sundae.java

--- Sundae.java
+++ Sundae.java
@@ -1,5 +1,5 @@
 /**
- * @author Group 3 - Victorira T. Tien N., Nik K. due date: Feb 17, 2021
+ * @author XiuNhon
  *
  *         subclass Sundae inherited from class IceCream, create getter methods
  *         for Sundae, override abstract method getCost to calculate IceCream
